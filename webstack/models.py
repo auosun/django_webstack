af2c3d932b1f2226dd27f4693af2aaa0f1e5f380
jinzhengyang <jzysuzhou@gmail.com>
2020-09-11 13:38:17 +0800

    update models.py

--- webstack/models.py
+++ webstack/models.py
@@ -7,8 +7,8 @@ class Menu(models.Model):
         verbose_name_plural = verbose_name
 
     id = models.AutoField(primary_key=True)
-    name = models.TextField(verbose_name='分组名')
-    icon = models.TextField(default='linecons-star',verbose_name='分组图标')
+    name = models.CharField(max_length=255,verbose_name='分组名')
+    icon = models.CharField(max_length=255,default='linecons-star',verbose_name='分组图标')
     weight = models.IntegerField(default=1,verbose_name='分组权重')
 
     def __str__(self):
@@ -21,10 +21,10 @@ class Website(models.Model):
         verbose_name_plural = verbose_name
 
     id = models.AutoField(primary_key=True)
-    name = models.TextField(verbose_name='网站名称')
-    description = models.TextField(verbose_name='网站描述',default='')
+    name = models.CharField(max_length=255,verbose_name='网站名称')
+    description = models.CharField(max_length=255,verbose_name='网站描述')
     url = models.URLField(verbose_name='网站链接')
-    title = models.TextField(verbose_name='原始标题')
+    title = models.CharField(max_length=255,verbose_name='原始标题')
     icon = models.URLField(verbose_name='网站图标',default='https://aupicgo.oss-cn-shanghai.aliyuncs.com/img/20200910125957.ico')
     weight = models.IntegerField(verbose_name='网站权重',default=1)
     menu = models.ForeignKey(Menu, on_delete=models.SET_DEFAULT,default=1)
